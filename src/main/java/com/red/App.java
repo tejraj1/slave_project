abd5abed14b84d7a3517b5f835cb4b41f55d06c8
tejraj1 <tejamadas@gmail.com>
2017-11-21 12:38:34 +0000

    slave build

---
+++ src/main/java/com/red/App.java
@@ -0,0 +1,13 @@
+package com.red;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
